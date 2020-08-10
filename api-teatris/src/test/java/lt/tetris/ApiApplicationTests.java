5292c33e529734e265e02c470bef556e7fe9dfa5
Edvardas3643 <Edvardas3643@gmail.com>
2020-08-10 19:53:15 +0300

    Initial commit

---
+++ api-teatris/src/test/java/lt/tetris/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.tetris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
